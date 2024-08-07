851e7b3f1392dab0bf73e4b85dc488bdcf0785ee
98308289 <159421591+98308289@users.noreply.github.com>
2024-08-07 19:07:50 +0530

    Delete sumofdigits.py

--- sumofdigits.py
+++
@@ -1,8 +0,0 @@
-def sum_of_digits(num):
-    total = 0
-    while num > 0:
-        total += num % 10
-        num //= 10
-    return total
-x=int(input("enter  a number:"))
-print(sum_of_digits(x))
